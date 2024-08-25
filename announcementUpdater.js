2bca721455d333b4dca97d7baec9bcb33c221aa9
Joysa Jain <joysajain@Joysas-MacBook-Air.local>
2024-08-25 16:15:46 +0530

    update codebenders

--- announcementUpdater.js
+++ announcementUpdater.js
@@ -1,5 +1,5 @@
 // Array of URLs for each society's announcement page
-const announcementPages = ["./announcement_tarannum.html", "./announcements_avira.html", "./announcement_hypnotics.html", "./announcement_innerve.html", "./announcement_igdtuwResource.html", "./bhav_announcement.html", "./announcement_nirvana.html", "./announcement_ai.html", "./announcement_ieee.html", "./announcements_enactus.html", "./announcement_avasa.html", "./announcement_synergy.html", "./announcement_protege.html", "./announcements_rtr.html", "./announcements_soch.html", "./announcement_rooh.html", "./announcementRobolution.html", "./announcement_msc.html", "./announcement_optica.html", "./announcements_leanin.html", "./announcement_gdsc.html", "./announcement_techneeds.html", "./announcement_technoliterati.html", "./minerva_announcement.html", "./announcement_hackclub.html", "./announcement_assert.html", "./announcement_instinct.html", "./announcement_finivesta.html", "./announcement_greensphere.html", "./announcement_accordo.html", "./announcements_ignite.html", "./announcementTaarangana.html", "./announcement_zena.html", "./announcement_rahnuma.html", "./announcement_prekshya.html"];
+const announcementPages = ["./announcement_tarannum.html", "./announcements_avira.html", "./announcement_hypnotics.html", "./announcement_innerve.html", "./announcement_igdtuwResource.html", "./bhav_announcement.html", "./announcement_nirvana.html", "./announcement_ai.html", "./announcement_ieee.html", "./announcements_enactus.html", "./announcement_avasa.html", "./announcement_synergy.html", "./announcement_protege.html", "./announcements_rtr.html", "./announcements_soch.html", "./announcement_rooh.html", "./announcementRobolution.html", "./announcement_msc.html", "./announcement_optica.html", "./announcements_leanin.html", "./announcement_gdsc.html", "./announcement_techneeds.html", "./announcement_technoliterati.html", "./minerva_announcement.html", "./announcement_hackclub.html", "./announcement_assert.html", "./announcement_instinct.html", "./announcement_finivesta.html", "./announcement_greensphere.html", "./announcement_accordo.html", "./announcements_ignite.html", "./announcementTaarangana.html", "./announcement_zena.html", "./announcement_rahnuma.html", "./announcement_coding.html"];
 
 // Function to load announcements from each page
 async function loadAnnouncements() {
@@ -81,7 +81,7 @@ function getSocietyName(page) {
         "./announcements_rtr.html": "Rotaract",
         "./announcements_soch.html": "Soch",
         "./announcement_rooh.html": "Rooh",
-
+        "./announcement_coding.html": "CodeBenders",
         "./announcementRobolution.html": "Robolution",
         "./announcement_msc.html": "MSC",
         "./announcement_optica.html": "Optica",
